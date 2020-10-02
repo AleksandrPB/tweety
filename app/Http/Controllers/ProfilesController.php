fc129cdc7bfadbc7ae1c7a282fb775e3f1b70356
Aleksandr Burlak <aleksandrburlak@icloud.com>
2020-10-02 12:23:08 +0300

    added new method update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ProfilesController extends Controller
 {
@@ -12,9 +13,50 @@ class ProfilesController extends Controller
         return view('profiles.show', compact('user'));
     }
 
-    protected function edit(User $user)
+    public function edit(User $user)
     {
 //        $this->authorize('edit', $user);
         return view('profiles.edit', compact('user'));
     }
+
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+            'username' => [
+                'string',
+                'required',
+                'max:255',
+                Rule::unique('users')->ignore($user),
+                'alpha_dash'
+            ],
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'avatar' => [
+                'required',
+                'file'
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user)
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'confirmed'],
+        ]);
+
+        $attributes['avatar'] = request('avatar')->store('avatars');
+
+        $user->update($attributes);
+
+        return redirect($user->path());
+    }
+
 }
