c766dd3b1089acd9f50eb4f37d2c14dbb5ddb464
Aleksandr Burlak <aleksandrburlak@icloud.com>
2020-10-03 10:07:42 +0300

    revised method update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -35,7 +35,6 @@ class ProfilesController extends Controller
                 'max:255'
             ],
             'avatar' => [
-                'required',
                 'file'
             ],
             'email' => [
@@ -52,7 +51,9 @@ class ProfilesController extends Controller
                 'confirmed'],
         ]);
 
-        $attributes['avatar'] = request('avatar')->store('avatars');
+        if (request('avatar')) {
+            $attributes['avatar'] = request('avatar')->store('avatars');
+        }
 
         $user->update($attributes);
 
